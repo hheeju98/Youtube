2e32cb8e1c218b39d9c60daad34bb9133b0475f0
hheeju98 <hheeju98@naver.com>
2023-03-22 23:39:30 +0900

    라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  return;
-  <></>;
+  return (
+    <>
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
