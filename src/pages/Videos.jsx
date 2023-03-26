3cfca732d0fa7454601dfc50301aca1dad033d19
hheeju98 <hheeju98@naver.com>
2023-03-26 20:29:27 +0900

    비디오 카드 만들기 -Context 사용

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -4,17 +4,16 @@ import { useParams } from "react-router-dom";
 import VideoCard from "../components/VideoCard";
 import Youtube from "../api/youtube";
 import FakeYoutube from "../api/fakeYoutube";
+import { useYoutubeApi } from "../context/YoutubeApiContext";
 
 export default function Videos() {
   const { keyword } = useParams();
+  const { youtube } = useYoutubeApi();
   const {
     isLoading,
     error,
     data: videos,
-  } = useQuery(["videos", keyword], () => {
-    const youtube = new Youtube();
-    return youtube.search(keyword);
-  });
+  } = useQuery(["videos", keyword], () => youtube.search(keyword));
   return (
     <>
       <div>Videos {keyword ? `${keyword}` : "hot"}</div>
